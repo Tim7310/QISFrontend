c9a07fae1127191c039eb3408926010426d8b41e
QuestInterns <questphi.interns@gmail.com>
2019-08-20 17:00:44 +0800

    microscopy layout

--- src/app/laboratory/microscopy-form/microscopy-form.component.ts
+++ src/app/laboratory/microscopy-form/microscopy-form.component.ts
@@ -8,6 +8,15 @@ import { MathService } from 'src/app/services/math.service';
 })
 export class MicroscopyFormComponent implements OnInit {
 
+  urineColor = ["STRAW", "LIGHT YELLOW", "YELLOW", "DARK YELLOW", "RED", "ORANGE", "AMBER"];
+  urineTransparency = ["CLEAR", "HAZY", "SL. TURBID", "TURBID"];
+  ucValue = ["NEGATIVE", "TRACE", "1+", "2+", "3+", "4+"];
+  ucpH  = ["5.0", "6.0", "6.5", "7.0", "7.5", "8.0", "8.5", "9.0", "9.5"];
+  ucSPG = ["1.000", "1.005", "1.010", "1.015", "1.020", "1.025", "1.030",];
+  microscopicVal = ["RARE", "FEW", "MODERATE", "MANY"];
+
+  fecColor = ["GREEN", "YELLOW", "LIGHT BROWN", "BROWN", "DARK BROWN"];
+  fecCons = ["FORMED", "SEMI-FORMED", "SOFT", "WATERY", "SLIGHTLY MUCOID", "MUCOID"]
   constructor(
     private math: MathService
   ) { 
